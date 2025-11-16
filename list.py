61ef51488bd9891bb56e238b69dee2a5bacb2d51
shivam <shivam984861@gmail.com>
2025-11-16 09:45:54 +0530

    new changes

---
+++ list.py
@@ -0,0 +1,6 @@
+num = [23, 45, 67, 89, 12, 90, 34, 56, 78, 11]
+print(type(num))
+print(len(num))
+print(num[0])   # prints the first element of the list
+print(num[4])   # prints the fifth element of the list
+print(num)
