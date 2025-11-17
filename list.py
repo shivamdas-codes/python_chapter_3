4680d12507053e32113868dc9f6af93913347213
shivam <shivam984861@gmail.com>
2025-11-17 22:23:06 +0530

    new changes

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+# example: 1
 num = [23, 45, 67, 89, 12, 90, 34, 56, 78, 11]
 print(type(num))
 print(len(num))
@@ -6,14 +7,19 @@ print(num[4])   # prints the fifth element of the list
 print(num)
 
 
+# example: 2
 student_info = ["shivam", 99.0, 10, "hyderabad" ]
 print(student_info)
 # we can access many values in a single list for example: str(), int() etc....
 
 
+# example: 3
 student = ["shivam", 99.5, 18, "hyderabad"]
-print(student[0])
+print(student[3])
+print(student[0],student[1])
 student[3] = "USA"
 print(student)
 # we can able to print any index that are present in the list and we can able to change the index according to the sitution 
 # if we try to print index which is not there in the list then it will show as error
+
+
