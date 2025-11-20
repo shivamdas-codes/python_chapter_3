75cdf7ee7dadcdfed5babf945d355ad48947da84
shivam <shivam984861@gmail.com>
2025-11-20 22:29:06 +0530

    new changes

---
+++ tuples.py
@@ -0,0 +1,17 @@
+# eg: 1
+tup = (3,4,5,6,8) #tuple values
+print(type(tup))  #type of tuple
+print(tup[0])
+print(tup[1])
+print(tup)
+# tup[2] = 10    this represents assign or replace a new value with the old value, which is not valid in the tuples and immideatly it returns error.
+
+
+# eg: 2
+tup1 = () #an empty tuple
+print(type(tup1))
+print(tup1)
+# this is absolutly valid in the tuples, we can assign the empty value in the tuples
+
+
+# eg: 3
