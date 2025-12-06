45c39fd75a4a411dd11c57d4b647d2294d36001e
shivam <shivam984861@gmail.com>
2025-12-06 22:21:54 +0530

    new changes

--- random_questions.py
+++ random_questions.py
@@ -1,6 +1,9 @@
+# INFINITE LOOP EXAMPLE:
 temp = 0
 while temp <100:    # less than 100
     temp += 1       # increment by 1
     if temp == 100:
         temp = 0
 # this is basically an infinite loop which never ends because when temp reaches 100 it is again assigned to 0.
+
+
