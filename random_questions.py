e0fa1e4c7e00b1a3baf204bb305edd8be49ec8cb
shivam <shivam984861@gmail.com>
2025-12-06 22:26:50 +0530

    statement added

--- random_questions.py
+++ random_questions.py
@@ -7,3 +7,9 @@ while temp <100:    # less than 100
 # this is basically an infinite loop which never ends because when temp reaches 100 it is again assigned to 0.
 
 
+# EXPONENTIATION OPERATOR:
+a = 2**3    # 2 raised to the power 3
+b = a**2    # a raised to the power 2
+print(b)
+c = 2**3**2 # 2 raised to the power (3 raised to the power 2)
+print(c)
