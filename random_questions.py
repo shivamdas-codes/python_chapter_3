6bb53b398ab7dd02274ed66552bc104810335ffb
shivam <shivam984861@gmail.com>
2025-12-11 22:16:38 +0530

    new changes

--- random_questions.py
+++ random_questions.py
@@ -1,96 +1,106 @@
-# (1).INFINITE LOOP EXAMPLE:
-temp = 0
-while temp <100:    # less than 100
-    temp += 1       # increment by 1
-    if temp == 100:
-        temp = 0
-# this is basically an infinite loop which never ends because when temp reaches 100 it is again assigned to 0.
+# # (1).INFINITE LOOP EXAMPLE:
+# temp = 0
+# while temp <100:    # less than 100
+#     temp += 1       # increment by 1
+#     if temp == 100:
+#         temp = 0
+# # this is basically an infinite loop which never ends because when temp reaches 100 it is again assigned to 0.
 
 
-# (2).EXPONENTIATION OPERATOR:
-a = 2**3    # 2 raised to the power 3
-b = a**2    # a raised to the power 2
-print(b)
-c = 2**3**2 # 2 raised to the power (3 raised to the power 2)
-print(c)
+# # (2).EXPONENTIATION OPERATOR:
+# a = 2**3    # 2 raised to the power 3
+# b = a**2    # a raised to the power 2
+# print(b)
+# c = 2**3**2 # 2 raised to the power (3 raised to the power 2)
+# print(c)
 
 
-# (3).BOOLEAN ARITHMETIC:
-x = [True,True,(True+1)*4]  #true =1
-y = False*3 + 1 #false=0
-for i in range(y+sum(x)-9):
-    print(sum(x)+i)
+# # (3).BOOLEAN ARITHMETIC:
+# x = [True,True,(True+1)*4]  #true =1
+# y = False*3 + 1 #false=0
+# for i in range(y+sum(x)-9):
+#     print(sum(x)+i)
 
 
-# (4).STRING SLICING:
-s = "butterfly" # index started from 0 to 8
-s = s[2:]   # index sliced from 2 to end
-# now this become "tterfly"
-s = s[:5] + s[-2]
-# now take the first 5 char and last 2 char (this become "tterf" + "l" = "tterfl")
-print(s)
+# # (4).STRING SLICING:
+# s = "butterfly" # index started from 0 to 8
+# s = s[2:]   # index sliced from 2 to end
+# # now this become "tterfly"
+# s = s[:5] + s[-2]
+# # now take the first 5 char and last 2 char (this become "tterf" + "l" = "tterfl")
+# print(s)
 
 
-# (5).FIND METHOD AND STRING SLICING:
-string = "python"
-print(string[string.find('t') + 3:])  # find the index of 't' and slice from that index +3 to end
+# # (5).FIND METHOD AND STRING SLICING:
+# string = "python"
+# print(string[string.find('t') + 3:])  # find the index of 't' and slice from that index +3 to end
 
 
-# (6).BOOLEAN EVALUATION:
-a = "1" # non empty string
-b = "0" # non empty string
-# the above assign values are string type that means both a and b are "True" because non-empty strings are considered True and empty strings are considered False in boolean context.
-# if any interger is assign then assume "true = 1 , false = 0"
-if a and b == True:
-    print("its a true statement")
-else:
-    print("its a false statement")
+# # (6).BOOLEAN EVALUATION:
+# a = "1" # non empty string
+# b = "0" # non empty string
+# # the above assign values are string type that means both a and b are "True" because non-empty strings are considered True and empty strings are considered False in boolean context.
+# # if any interger is assign then assume "true = 1 , false = 0"
+# if a and b == True:
+#     print("its a true statement")
+# else:
+#     print("its a false statement")
 
 
-# (7).TYPE CONVERSION:
-a = int("11", 2) # binary to decimal conversion
-print(a)
+# # (7).TYPE CONVERSION:
+# a = int("11", 2) # binary to decimal conversion
+# print(a)
 
 
-# (8).LIST REMOVAL DURING ITERATION:
-my_list = [3,4,5,6,7] #THE ELEMENTS ARE ASSIGNED FROM INDEX 0 TO 4
-for i in my_list:
-# THIS LOOP FOLLOWS THE ELEMENTS ONE BY ONE FROM THE LIST AND REMOVES THEM
-# EXAMPLE: my_list[0], my_list[1], my_list[2]....
-    my_list.remove(i)
-print(my_list)
+# # (8).LIST REMOVAL DURING ITERATION:
+# my_list = [3,4,5,6,7] #THE ELEMENTS ARE ASSIGNED FROM INDEX 0 TO 4
+# for i in my_list:
+# # THIS LOOP FOLLOWS THE ELEMENTS ONE BY ONE FROM THE LIST AND REMOVES THEM
+# # EXAMPLE: my_list[0], my_list[1], my_list[2]....
+#     my_list.remove(i)
+# print(my_list)
 
 
-# (9).FOR LOOP WITH RANGE AND ACCUMULATION:
-x = 0
-for i in range(3):#the range(0,1,2) now add one after one with x
-# case(1): i=0 , x=0+0=0
-# case(2): i=1 , x=0+1=1
-# case(3): i=2 , x=1+2=3      finally i=2 and x=3
-    x += i
-print(i,x)
+# # (9).FOR LOOP WITH RANGE AND ACCUMULATION:
+# x = 0
+# for i in range(3):#the range(0,1,2) now add one after one with x
+# # case(1): i=0 , x=0+0=0
+# # case(2): i=1 , x=0+1=1
+# # case(3): i=2 , x=1+2=3      finally i=2 and x=3
+#     x += i
+# print(i,x)
 
 
-# (10).VARIABLE ASSIGNMENT AND MUTABILITY:
-x = 0
-y = x   # y is assigned the value of x which is 0
-y += 1  # y is incremented by 1, now y=1 but x remains 0
-print(x,y)
+# # (10).VARIABLE ASSIGNMENT AND MUTABILITY:
+# x = 0
+# y = x   # y is assigned the value of x which is 0
+# y += 1  # y is incremented by 1, now y=1 but x remains 0
+# print(x,y)
 
 
-# (11).LIST SLICING WITH NEGATIVE STEP:
-"""doubt"""
-var = [1,2,3,4] #original list
-new_var = var[::-2] #[start:end:step] follow this pattern if there is three slicing and the step is negative in this case which is -2.
-new_var.append(9)
-print(var)
-print(new_var)
+# # (11).LIST SLICING WITH NEGATIVE STEP:
+# """doubt"""
+# var = [1,2,3,4] #original list
+# new_var = var[::-2] #[start:end:step] follow this pattern if there is three slicing and the step is negative in this case which is -2.
+# new_var.append(9)
+# print(var)
+# print(new_var)
 
 
 # # (12).STRING METHOD isalpha():
-a = "12"    #its a string not a number
-if a.isalpha(): # checks if all characters in the string are alphabetic eg: 'abc' = true , '123' = false , 'abc123' = false
-    print("true")
-else:
-    print("false")
+# a = "12"    #its a string not a number
+# if a.isalpha(): # checks if all characters in the string are alphabetic eg: 'abc' = true , '123' = false , 'abc123' = false
+#     print("true")
+# else:
+#     print("false")
 
+
+# (13).NESTED CONDITIONAL STATEMENTS:
+num = 75
+if num % 5 == 0:
+    if num % 2 == 0:
+        print("java")
+    else:
+        print("python")
+else:
+    print("c++")
