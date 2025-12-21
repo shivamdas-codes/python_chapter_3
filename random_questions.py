024a34fd7edde0305ffa4fc11fdcf1c3f177f942
shivam <shivam984861@gmail.com>
2025-12-21 22:39:26 +0530

    initial changes

--- random_questions.py
+++ random_questions.py
@@ -142,3 +142,12 @@
 letters = ['a', 'b', 'c', 'd']
 combo = '-'.join(letters).upper()
 print(combo)
+
+
+# (21).LIST MUTATION DURING ITERATION:
+clcoding = [1,2,3,4]
+total = 0
+for x in clcoding:
+    total += x
+    clcoding[0] = total 
+print(clcoding)
